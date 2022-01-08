5e5ffc291b0c885ddf1e6c0d355e0f0707db6633
wgrzesiak <wit@grzesiak.eu>
2022-01-08 22:47:06 +0100

    formated covariance_matrix.py

--- covariance_matrix.py
+++ covariance_matrix.py
@@ -2,8 +2,9 @@ import pandas as pd
 
 df = pd.DataFrame(
     {"Age": [25, 32, 37],
-        "Expeorence": [2, 6, 9],
-        "Salary": [2000, 3000, 3500]}
+     "Expeorence": [2, 6, 9],
+     "Salary": [2000, 3000, 3500]}
 )
+
 cov_matrix = df.cov()
 print(cov_matrix)
